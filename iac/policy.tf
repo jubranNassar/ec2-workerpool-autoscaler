b083587e2685459fc655624ca60b60564f056d50
Jubran Nassar <84308658+jubranNassar@users.noreply.github.com>
2024-02-23 16:13:41 -0500

    Update policy.tf

--- iac/policy.tf
+++ iac/policy.tf
@@ -62,8 +62,8 @@ resource "aws_iam_role" "lambda" {
   name               = "ec2-autoscaler-${var.worker_pool_id}"
   assume_role_policy = data.aws_iam_policy_document.assume_lambda_role.json
 
-  inline_policy {
-    name   = "ec2-autoscaler-${var.worker_pool_id}"
-    policy = data.aws_iam_policy_document.lambda_policy.json
-  }
+  # inline_policy {
+  # name   = "ec2-autoscaler-${var.worker_pool_id}"
+  # policy = data.aws_iam_policy_document.lambda_policy.json
+  #}
 }
